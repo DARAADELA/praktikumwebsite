f33c8ed89307002dbf7ae2e549e4e645ebd716b9
DARAADELA <daraadela60@gmail.com>
2024-10-02 14:34:11 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,12 @@
+document.writeln("<center>");
+document.writeln("hr width=600 color=black">);
+document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
+document.writeln("<h2>PENDIDIKAN TERNOLOGI INFORMASI<H2>");
+document.writeln("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM<H3>");
+document.writeln("<hr width=600 color=black>");
+documen.writeln("<center>");
+
+
+
+
+                 
